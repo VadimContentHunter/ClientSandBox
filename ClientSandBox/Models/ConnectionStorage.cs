8f87e7b31e29e7a45edb3dda94556237ab35d236
Vadim <project.k.vadim@gmail.com>
2026-07-15 00:19:12 +0300

    Рефакторинг: добавлен класс ConnectionStatuses со статусами
    Удалён неиспользуемый код и пространства имён из ConnectionStorage.cs. Вместо класса ConnectionStorage добавлен статический класс ConnectionStatuses с константами для различных статусов подключения и XML-комментарием.

--- ClientSandBox/Models/ConnectionStorage.cs
+++ ClientSandBox/Models/ConnectionStorage.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace ClientSandBox.Models;
 
-namespace ClientSandBox.Models
+/// <summary>
+/// Статусы подключения.
+/// </summary>
+public static class ConnectionStatuses
 {
-    internal class ConnectionStorage
-    {
-    }
+    public const string Ready = "✔ Готов";
+
+    public const string InvalidJson = "✖ Некорректный JSON";
+
+    public const string MissingTag = "✖ Отсутствует tag";
+
+    public const string MissingType = "✖ Отсутствует type";
+
+    public const string DuplicateTag = "✖ Дублирующийся tag";
+
+    public const string MultipleTun = "✖ Допускается только один TUN";
+
+    public const string PortBusy = "⚠ Порт занят";
 }
